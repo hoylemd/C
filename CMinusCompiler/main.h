518d56ee27d4eb2d473e70c94c899d7037236645
Michael D. Hoyle <hoylemd@gmail.com>
2012-01-07 11:46:40 -0500

    cleaned up main.h

--- CMinusCompiler/main.h
+++ CMinusCompiler/main.h
@@ -1,16 +1,22 @@
+/**
+ * Header file for the giant main code file
+ */
+
 #include <stdio.h>
 #include "globals.h"
 
+/* define register shorthands */
 #define AC 0
 #define AC1 1
 #define OP1 2
 #define OP2 3
-#define RES	4
+#define RES 4
 #define PC 7
 #define GP 6
 #define FP 5
 #define LABELSIZE 128
-/* declarastion of parsing fields */
+
+/* declaration of parsing fields */
 char tokenString[50];
 int lineno = 0;
 
@@ -19,7 +25,7 @@ FILE * source = NULL;
 FILE * out = NULL;
 FILE * error = NULL;
 
-/* flag for if the AST will be printed */
+/* flag for if the AST or symbol table will be printed */
 int PrintTreeFlag = 0;
 int PrintTableFlag = 0;
 
@@ -38,69 +44,98 @@ extern TreeNode * AST;
 /*pointer to the error flag*/
 extern int errorEncountered;
 
+/* enum for the instruction type */
 typedef enum
 {
 	RegisterInstruction,
 	RegisterMemoryInstruction
 } InstructionType;
 
+/* enum for register-only opcodes */
 typedef enum
 {
 	NoneR,
-	/* Register Only */
 	Halt, Input, Output,
 	Add, Subtract, Multiply, Divide
 } rOpCode;
 
+/* enum for register / memory opcodes */
 typedef enum
 {
 	NoneRM,
-	/* Register / Memory */
 	Load, LoadAddress, LoadConstant,
 	Store,
 	JumpLessThan, JumpLessEqual, JumpGreaterThan, JumpGreaterEqual, JumpEqual, JumpNotEqual
 } rmOpCode;
 
+/* union for all types of opcodes */
 typedef union
 {
 	rOpCode rCode;
 	rmOpCode rmCode;
 } opCode;
 
+/* structure to build an instruction */
 typedef struct instr
 {
+    /* operation type and code */
 	InstructionType type;
 	opCode oc;
+
+    /* operands */
 	int o1;
 	int o2;
 	int o3;
+
+    /* list pointers */
 	struct instr * prev;
 	struct instr * next;
+
+    /* line number */
 	int num;
+
+    /* label/comment */
 	char label[LABELSIZE];
 } Instruction;
 
+/* prototype for Instruction Constructor */
 Instruction * newInstruction(InstructionType, rOpCode, rmOpCode, int, int, int, char *);
 
+/* Instruction List structure */
 typedef struct instrList
 {
+    /* count of the contained instructions */
 	int numInstr;
+
+    /* list pointers */
 	Instruction * first;
 	Instruction * last;
 } InstructionList;
 
+/* structure for chunks of assembly code */
 typedef struct assemblyChunk
 {
+    /* preamble comment */
 	char preamble[256];
+
+    /* list of instructions */
 	InstructionList * iList;
+
+    /* postamble comment */
 	char postamble[256];
+
+    /* list pointers */
 	struct assemblyChunk * next;
 	struct assemblyChunk * prev;
 } AssemblyChunk;
 
+/* structure for code sections */
 typedef struct assemblyCode
 {
+    /* chunk list pointers */
 	AssemblyChunk * first;
 	AssemblyChunk * last;
+
+    /* counter for chunks */
 	int chunkCount;
 } AssemblyCode;
